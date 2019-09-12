e630f32fd1d20c5b0d0df6c362a9b4b66655dc42
Jared.Tan <jian.tan@daocloud.io>
2019-09-12 17:46:21 +0800

    update fro dx 2.3.0

--- service-a/src/main/java/io/daocloud/dmp/skywalking/performance/service/a/UserController.java
+++ service-a/src/main/java/io/daocloud/dmp/skywalking/performance/service/a/UserController.java
@@ -14,6 +14,6 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
-        return this.userRepository.findById(id).orElseThrow();
+        return this.userRepository.findById(id).orElse(null);
     }
 }
